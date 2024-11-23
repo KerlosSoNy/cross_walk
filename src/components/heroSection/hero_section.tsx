1624b748c1298687a70ddf1c55d5012277815620
kero <kerlossony@yahoo.com>
2024-11-23 13:28:17 +0200

    New Project For CrossWalk

---
+++ src/components/heroSection/hero_section.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GlobeDemo } from '../globalWorldComponent'
+
+export default function Hero_section() {
+    return (
+        <div className="mt-10">
+            <GlobeDemo />
+        </div>
+    )
+}
